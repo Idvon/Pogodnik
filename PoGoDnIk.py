e47bd52f1a21bdb8c000e42dc52f177a7ff261cd
Idvon <93034685+Idvon@users.noreply.github.com>
2023-02-23 19:30:48 +0300

    vbIvod temp ble (#1)
    * vbIvod temp ble
    
    * perfect visible weather
    
    * add check city name
    
    * add country and state

--- PoGoDnIk.py
+++ PoGoDnIk.py
@@ -4,15 +4,24 @@ import json
 with open("config.json", "r") as f:
     city = json.load(f)
 call_city = requests.get("https://api.openweathermap.org/geo/1.0/direct?"
-                         "q="+city["city_name"]+",ru&"
+                         f"q={city['city_name']}&"
                          "appid=2427c9b0a80567c0e8c21bdc1bd0b125")
 city_data = json.loads(call_city.text)
-city_data = city_data[0]
-lat = city_data["lat"]
-lon = city_data["lon"]
-call = requests.get("https://api.openweathermap.org/data/2.5/weather?"
-                    "lat="+str(lat)+"&lon="+str(lon)+"&"
-                    "appid=2427c9b0a80567c0e8c21bdc1bd0b125&"
-                    "units=metric&"
-                    "lang=ru")
-print(call.json())
+if len(city_data) == 0:
+    print("This city not found. Please, check city name.")
+else:
+    city_data = city_data[0]
+    lat = city_data['lat']
+    lon = city_data['lon']
+    call = requests.get("https://api.openweathermap.org/data/2.5/weather?"
+                        f"lat={lat}&lon={lon}&"
+                        "appid=2427c9b0a80567c0e8c21bdc1bd0b125&"
+                        "units=metric&")
+    data = call.json()
+    print(f"Weather in {city['city_name']}\n"
+          f"Country: {city_data['country']}\n"
+          f"State: {city_data['state']}\n"
+          f"Temperature: {data['main']['temp']} C\N{degree sign}\n"
+          f"Humidity: {data['main']['humidity']} %\n"
+          f"Wind speed: {data['wind']['speed']} m/s\n"
+          f"Wind direction: {data['wind']['deg']}\N{degree sign}")
