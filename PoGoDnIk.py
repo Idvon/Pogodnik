8c23e9e49da05d3373a0d669a605ba81c989ae7b
Idvon <d.suchanow2010@yandex.ru>
2023-02-24 22:21:56 +0300

    fisting

--- PoGoDnIk.py
+++ PoGoDnIk.py
@@ -9,7 +9,7 @@ def main():
     with open("config.json") as f:
         config_data = json.load(f)
     city = geocoding.geo(config_data)
-    if city == 0:
+    if city is None:
         return "This city is not found. Please, check city name"
     elif config_data['provider_name'] == "openweather":
         return conclusion.printing(open_weather.weather_data(city))
--- geocoding.py
+++ geocoding.py
@@ -1,14 +1,12 @@
 import requests
 import json
-from typing import Union
+from typing import Optional
 
 
-def geo(config_data: dict) -> Union[dict, int]:
+def geo(config_data: dict) -> Optional[dict]:
     call_city = requests.get("https://api.openweathermap.org/geo/1.0/direct?"
                              f"q={config_data['city_name']}&"
                              "appid=2427c9b0a80567c0e8c21bdc1bd0b125")
     city = json.loads(call_city.text)
     if len(city) != 0:
         return city[0]
-    else:
-        return 0
