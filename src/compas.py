16311d8daf20a64d60c373517a7e829a83ea6012
Idvon <d.suchanow2010@yandex.ru>
2023-02-25 22:04:57 +0300

    add compas

---
+++ src/compas.py
@@ -0,0 +1,17 @@
+def direction(deg: int) -> str:
+    if 22.5 <= deg < 67.5:
+        return "NE"
+    elif 67.5 <= deg < 112.5:
+        return "E"
+    elif 112.5 <= deg < 157.5:
+        return "SE"
+    elif 157.5 <= deg < 202.5:
+        return "S"
+    elif 202.5 <= deg < 247.5:
+        return "SW"
+    elif 247.5 <= deg < 292.5:
+        return "W"
+    elif 292.5 <= deg < 337.5:
+        return "NW"
+    else:
+        return "N"
--- src/conclusion.py
+++ src/conclusion.py
@@ -1,3 +1,4 @@
+from src.compas import direction
 from typing import Optional
 
 
@@ -11,5 +12,5 @@ def printing(data: Optional[dict]) -> str:
                f"Temperature: {data['temp']} \N{degree sign}C\n"\
                f"Humidity: {data['humidity']} %\n"\
                f"Wind speed: {data['windspd']} m/s\n"\
-               f"Wind direction: {data['winddeg']}\N{degree sign}\n"\
+               f"Wind direction: {direction(data['winddeg'])}\n"\
                f"By {data['provider']}"
