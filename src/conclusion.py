50bb026aa665b3cdb9a3b5ac02491a1d288f2d16
Idvon <d.suchanow2010@yandex.ru>
2023-02-28 23:09:24 +0300

    add csv export

--- src/conclusion.py
+++ src/conclusion.py
@@ -1,16 +1,25 @@
+import csv
+import os.path
 from src.compas import direction
-from typing import Optional
 
 
-def printing(data: Optional[dict]) -> str:
+def printing(data, out_name: [dict, str]) -> str:
     if data is None:
         return "Please, check api key"
     else:
-        return f"Weather in {data['city']}\n"\
-               f"Country: {data['country']}\n"\
-               f"State: {data['state']}\n"\
-               f"Temperature: {data['temp']} \N{degree sign}C\n"\
-               f"Humidity: {data['humidity']} %\n"\
-               f"Wind speed: {data['windspd']} m/s\n"\
-               f"Wind direction: {direction(data['winddeg'])}\n"\
+        if os.path.exists(out_name) is True:
+            f = open(out_name, "a", newline='')
+        else:
+            f = open(out_name, "w", newline='')
+        writer = csv.writer(f)
+        writer.writerow([key for key in data])
+        writer.writerow([data[key] for key in data])
+        f.close()
+        return f"Weather in {data['city']}\n" \
+               f"Country: {data['country']}\n" \
+               f"State: {data['state']}\n" \
+               f"Temperature: {data['temp']} \N{degree sign}C\n" \
+               f"Humidity: {data['hum']} %\n" \
+               f"Wind speed: {data['windspeed']} m/s\n" \
+               f"Wind direction: {direction(data['winddeg'])}\n" \
                f"By {data['provider']}"
--- src/open_meteo.py
+++ src/open_meteo.py
@@ -1,22 +1,26 @@
-import requests
+from requests import get
+from src.compas import direction
+import datetime
 
 
 def weather_data(city: dict) -> dict:
-    call = requests.get("https://api.open-meteo.com/v1/forecast?"
-                        f"latitude={city['lat']}&longitude={city['lon']}&"
-                        "current_weather=true&"
-                        "windspeed_unit=ms&"
-                        "hourly=relativehumidity_2m")
+    call = get("https://api.open-meteo.com/v1/forecast?"
+               f"latitude={city['lat']}&longitude={city['lon']}&"
+               "current_weather=true&"
+               "windspeed_unit=ms&"
+               "hourly=relativehumidity_2m")
     data = call.json()
     current_time = data['current_weather']['time']
     list_time = data['hourly']['time']
     index_humidity = list_time.index(current_time)
-    d = {'city': city['name'],
-         'country': city['country'],
+    d = {'datetime': datetime.datetime.now(datetime.timezone.utc),
+         'provider': 'openmeteo',
+         'city': city['name'],
          'state': city['state'],
+         'country': city['country'],
          'temp': data['current_weather']['temperature'],
-         'humidity': data['hourly']['relativehumidity_2m'][index_humidity],
-         'windspd': data['current_weather']['windspeed'],
+         'hum': data['hourly']['relativehumidity_2m'][index_humidity],
+         'winddir': direction(int(data['current_weather']['winddirection'])),
          'winddeg': int(data['current_weather']['winddirection']),
-         'provider': 'Open-Meteo'}
+         'windspeed': data['current_weather']['windspeed']}
     return d
