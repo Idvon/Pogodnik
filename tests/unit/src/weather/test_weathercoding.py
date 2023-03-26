d0186eb6a65b97a5028a4ed3538f6c05baede450
Dmitrii M <nameleeees@gmail.com>
2023-03-26 19:24:18 +0400

    add geocoding unittest

--- tests/unit/src/weather/test_weathercoding.py
+++ tests/unit/src/weather/test_weathercoding.py
@@ -1,31 +1,27 @@
+from pytest import fixture
+
 from src.weather.weathercoding import (
     OpenMeteoWeatherProvider,
     OpenWeatherWeatherProvider,
     create_net_weather_provider,
 )
-from tests.unit.src.weather.constants import COORDS, OM_RESPONSE, OW_RESPONSE
+from tests.conftest import MockResponse
+from tests.unit.constants import COORDS, OM_RESPONSE, OW_RESPONSE
+
+GET_PATH = "src.weather.weathercoding.get"
 
 
-# https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response
-# This method will be used by the mock to replace requests.get
-def mocked_get(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
-
-        def json(self):
-            return self.json_data
-
-    if args[0].startswith("https://api.openweathermap.org/data/2.5/weather?"):
-        return MockResponse(OW_RESPONSE, 200)
-    elif args[0].startswith("https://api.open-meteo.com/v1/forecast?"):
-        return MockResponse(OM_RESPONSE, 200)
-    return MockResponse(None, 404)
+@fixture
+def mock_openmeteo_get(mocker):
+    mocker.patch(GET_PATH, lambda _: MockResponse(OM_RESPONSE, 200))
 
 
-def test_openweather_parsing(mocker):
-    mocker.patch("src.weather.weathercoding.get", mocked_get)
+@fixture
+def mock_openweather_get(mocker):
+    mocker.patch(GET_PATH, lambda _: MockResponse(OW_RESPONSE, 200))
+
+
+def test_openweather_parsing(mock_openweather_get):
     provider = OpenWeatherWeatherProvider({"api_key": "beepboop"}, COORDS)
     assert isinstance(provider, OpenWeatherWeatherProvider)
     data = provider.weather_data(provider.request())
@@ -39,8 +35,7 @@ def test_openweather_parsing(mocker):
     }
 
 
-def test_openmeteo_parsing(mocker):
-    mocker.patch("src.weather.weathercoding.get", mocked_get)
+def test_openmeteo_parsing(mock_openmeteo_get):
     provider = OpenMeteoWeatherProvider({}, COORDS)
     assert isinstance(provider, OpenMeteoWeatherProvider)
     data = provider.weather_data(provider.request())
