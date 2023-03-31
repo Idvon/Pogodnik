574bd520195cbf53432eb3fe3551ea1748ccc8aa
Idvon <d.suchanow2010@yandex.ru>
2023-03-31 10:16:30 +0300

    first version db sqlite3

--- src/output/conclusion.py
+++ src/output/conclusion.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import sqlite3
 from pathlib import Path
 
 
@@ -25,3 +26,53 @@ def to_file(city_data: dict, out_file: Path):
         if headers is not None:
             writer.writerow(headers)
         writer.writerow(data.values())
+
+
+def sql_file(city_data: dict):
+    file = "db.sqlite3"
+    date = {"datetime": datetime.datetime.now(datetime.timezone.utc)}
+    data = date | city_data
+    data = tuple(data.values())
+    print(data)
+    print(type(data))
+    try:
+        sqlite_connection = sqlite3.connect(file)
+        headers = "create table if not exists weather_results (" \
+                  "date date," \
+                  "provider text," \
+                  "temp real," \
+                  "hum integer," \
+                  "winddir text," \
+                  "winddeg integer," \
+                  "windspeed real," \
+                  "city text," \
+                  "state text," \
+                  "country text);"
+        cursor = sqlite_connection.cursor()
+        print("DB connect to SQLite")
+        cursor.execute(headers)
+        cursor.execute("insert into weather_results(date, provider, temp, hum, winddir, winddeg, windspeed, city, state, country)"
+                       "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
+        sqlite_connection.commit()
+        print("DB creation")
+        cursor.close()
+    except sqlite3.Error as error:
+        print(f"Error connecting to DB {error}")
+    finally:
+        if (sqlite_connection):
+            sqlite_connection.close()
+            print("Connecting with SQLite close")
+
+
+if __name__ == "__main__":
+    q = {"datetime": datetime.datetime.fromisoformat("2023-03-26 15:56:47.080217+00:00"),
+         "provider": "openweather",
+         "temp": 2.04,
+         "hum": 93,
+         "winddir": "NE",
+         "winddeg": 50,
+         "windspeed": 4,
+         "city": "Saint Petersburg",
+         "state": "Saint Petersburg",
+         "country": "RU"}
+    sql_file(q)
