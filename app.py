b5bcac784bfa702d1bf9794784fd44e4f899ec9d
Idvon <d.suchanow2010@yandex.ru>
2023-06-06 22:22:21 +0300

    add net data

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
+import json
 from os import getenv
 from pathlib import Path
 
-from flask import Flask, render_template
-from wtforms import Form, StringField, validators
+from flask import Flask, render_template, request
+from wtforms import Form, StringField
 from PoGoDnIk import parser_files
 
 APP = Flask(__name__)
 
 
 class MyForm(Form):
-    first = StringField('City Name', [validators.Length(min=3, max=30)])
+    city_name = StringField('City Name')
 
 
-@APP.route("/")
+@APP.route("/", methods=["GET", "POST"])
 def web_conclusion():
+    form = MyForm(request.form)
     file_config = Path(getenv("FILE_CONFIG"))
+    with open(file_config) as f:
+        confit_data = json.load(f)
     file_output = Path(getenv("FILE_OUTPUT"))
-    city_config = parser_files(file_config, file_output)    # sending work files
-    return render_template("index.html", data=city_config)
+    if request.method == "POST":
+        city_name = request.form.get('city_name')
+        confit_data['city_name'] = city_name
+        file_config = Path("net_config.json")
+        with open(file_config, "a") as f:
+            json.dump(confit_data, f)
+        return render_template("data.html", data=parser_files(file_config, file_output))
+    return render_template("index.html", city_name=form)
